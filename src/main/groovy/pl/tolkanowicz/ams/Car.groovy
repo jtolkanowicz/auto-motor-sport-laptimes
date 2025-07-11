ad3cbea0f4bc9cabae1dc55128e27f8cf6189ebf
Anonymous <13221247+jtolkanowicz@users.noreply.github.com>
2025-07-11 15:23:47 +0200

    Added tyre spec, refactored code

--- src/main/groovy/pl/tolkanowicz/ams/Car.groovy
+++ src/main/groovy/pl/tolkanowicz/ams/Car.groovy
@@ -9,9 +9,12 @@ class Car {
 
     Integer id
 
-    String make, model, productionYears, nordschleifeTime, hockenheimTime, url, testTitle, driver, gearbox, layout, testDate
+    String make, model, productionYears, nordschleifeTime, hockenheimTime, url, testTitle, driver, gearbox, layout,
+            testDate, tyres, tyresSource, tyresSpec
 
     Integer weight, power, torque
 
-    Float time100, time200
+    Double time100, time200
+
+    Boolean optionalTyre
 }
