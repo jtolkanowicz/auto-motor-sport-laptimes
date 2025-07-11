8903204824b4973b3996f9332de1da51038c5316
Anonymous <13221247+jtolkanowicz@users.noreply.github.com>
2025-07-11 15:22:41 +0200

    Added saving single car to MongoDB, small refactor of Car and CarPage

--- src/main/groovy/ParseSingleCar.groovy
+++ src/main/groovy/ParseSingleCar.groovy
@@ -1,3 +1,8 @@
+import groovy.json.JsonOutput
+import org.bson.Document
+import pl.tolkanowicz.ams.Car
+import pl.tolkanowicz.ams.mongo.CarMongo
+import pl.tolkanowicz.ams.mongo.Connection
 import pl.tolkanowicz.ams.pages.CarPage
 import pl.tolkanowicz.ams.pages.LoginPage
 
@@ -5,8 +10,17 @@ import pl.tolkanowicz.ams.pages.LoginPage
  * Created by jacek on 15.02.17.
  */
 new LoginPage().login()
-CarPage carPage = new CarPage(url: "/supertest/lamborghini-huracan-coupe-lp-610-4-supertest-11494782.html")
-if(carPage.hasCarData()){
-    println carPage.getCarData()
+String url = "/supertest/lamborghini-huracan-coupe-lp-610-4-supertest-11494782.html"
+Car car = new Car(url)
+CarMongo carMongo = new CarMongo()
+if(!carMongo.carExists(car.id)){
+    CarPage carPage = new CarPage(car)
+    if (carPage.hasCarData()) {
+        carPage.readData()
+        carMongo.saveOrUpdateCar(car)
+    }
 }
+String json = carMongo.getCar(car.id).toJson()
+System.out.println(JsonOutput.prettyPrint(json))
+
 
--- src/main/groovy/pl/tolkanowicz/ams/Car.groovy
+++ src/main/groovy/pl/tolkanowicz/ams/Car.groovy
@@ -7,6 +7,13 @@ import java.time.YearMonth
  */
 class Car {
 
+    public Car(String url){
+        int lastDashId = url.lastIndexOf("-")
+        int lastDotId = url.lastIndexOf(".")
+        id = Integer.parseInt(url.substring(lastDashId+1, lastDotId))
+        this.url = url
+    }
+
     Integer id
 
     String make, model, productionYears, nordschleifeTime, hockenheimTime, url
