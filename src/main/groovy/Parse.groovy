7f5a4f6355c7c1bdb1ed0e84d4959da4380490ae
Anonymous <13221247+jtolkanowicz@users.noreply.github.com>
2025-07-11 15:22:41 +0200

    Added iterating through test list, refactor of car parsing

--- src/main/groovy/Parse.groovy
+++ src/main/groovy/Parse.groovy
@@ -1,10 +1,11 @@
 import geb.Browser
 import geb.navigator.Navigator
+import pl.tolkanowicz.ams.Car
 import pl.tolkanowicz.ams.pages.CarPage
 import pl.tolkanowicz.ams.pages.LoginPage
-import pl.tolkanowicz.ams.Car
+import pl.tolkanowicz.ams.pages.SupertestPage
 
-Browser logged = Browser.drive {
+Browser.drive {
     to LoginPage
 
     username.value("sadrascal@mailinator.com")
@@ -21,27 +22,14 @@ Browser logged = Browser.drive {
         title == "Mein Profil - AUTO MOTOR UND SPORT"
         //js.exec"return document.readyState" == "complete"
     }
-    /*
-    to CarPage
-    Navigator carName = $("th", 1)
-    String make = "";
-    String model = "";
-    if(!carName.isEmpty()){
-        String temp = carName.text()
-        make = temp.take(temp.indexOf(" "))
-        model = temp.drop(temp.indexOf(" ")+1)
-    }
-
-    String time = "0:00"
-    Navigator nordschleifeSection = $("td", text: "Nordschleife").parent().parent().children()
-    while(nordschleifeSection.size()>0){
-        if(nordschleifeSection.children().getAt(0).text().equals("Rundenzeit")){
-            time = nordschleifeSection.children().getAt(1).text()
-        }
-        nordschleifeSection = nordschleifeSection.next()
-    }
-    Car car = new Car(make: make, model: model, time: time)
-    print car*/
 }
-Car car = new Car()
-logged = CarPage.getCarData(logged, car)
+List<String> urls = SupertestPage.getSupertestUrls()
+List<Car> cars = new ArrayList<>()
+for(String url : urls) {
+    Car car = CarPage.getCarData(url)
+    if(car != null){
+        cars.add(car)
+    }
+}
+println cars
+//logged = CarPage.getCarData(logged, car)
