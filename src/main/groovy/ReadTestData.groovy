6bd5552c6a7ac6ffad93c65af62fbbfa0b83af23
Anonymous <13221247+jtolkanowicz@users.noreply.github.com>
2025-07-11 15:23:47 +0200

    Refactored reading test data, added new variables to parser

--- src/main/groovy/ReadTestData.groovy
+++ src/main/groovy/ReadTestData.groovy
@@ -1,28 +1,21 @@
-import geb.Browser
-import geb.navigator.Navigator
-import groovy.json.JsonBuilder
-import groovy.json.JsonSlurper
 import pl.tolkanowicz.ams.Car
+import pl.tolkanowicz.ams.TestLink
 import pl.tolkanowicz.ams.mongo.CarMongo
+import pl.tolkanowicz.ams.mongo.TestLinkMongo
 import pl.tolkanowicz.ams.pages.CarPage
 import pl.tolkanowicz.ams.pages.LoginPage
-import pl.tolkanowicz.ams.pages.SupertestPage
 
+TestLinkMongo testLinkMongo = new TestLinkMongo()
+CarMongo carMongo = new CarMongo()
+List<TestLink> testLinks = testLinkMongo.getAllLinksWithTestData()
+Integer count = testLinks.size()
 new LoginPage().login()
-File testLinks = new File('Links.json')
-JsonSlurper slurper = new JsonSlurper()
-String jsonText = testLinks.getText()
-List<String> links = slurper.parseText( jsonText )
-
-links.eachWithIndex{ url, idx ->
-    Car car = new Car(url)
-    CarMongo carMongo = new CarMongo()
-    if(!carMongo.carExists(car.id)){
-        CarPage carPage = new CarPage(car)
-        if (carPage.hasCarData()) {
-            carPage.readData()
-            carMongo.saveOrUpdateCar(car)
-        }
+testLinks.eachWithIndex{ TestLink link, int i ->
+    CarPage carPage = new CarPage(link)
+    Car car = carPage.car
+    if(!carMongo.carExists(car.id) && carPage.hasTestData()) {
+        println "Current id=$i/$count; article url $link.url"
+        carPage.readData()
+        carMongo.createCar(car)
     }
-    println "Current id=$idx"
 }
